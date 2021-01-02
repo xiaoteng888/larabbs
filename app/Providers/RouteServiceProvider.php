23cb15580b91f32a01e689aa6926bda83d2dfd7d
heman <605184327@qq.com>
2021-01-02 10:19:12 +0000

    修复跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,10 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
-
-    protected $namespace = 'App\Http\Controllers';
     /**
      * The controller namespace for the application.
      *
@@ -28,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+     protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
