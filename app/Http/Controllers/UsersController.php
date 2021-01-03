43fb7879c35fa3889ef0b7ccbf2b6734acd6e056
heman <605184327@qq.com>
2021-01-03 07:01:26 +0000

    用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function show(User $user)
+    {
+       return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+       return view('users.edit');	
+    }
+
+    public function update(User $user)
+    {
+    	
+    }
+
+}
