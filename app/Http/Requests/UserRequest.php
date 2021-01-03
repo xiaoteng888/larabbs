d2e928cdbc8f3e023e9ac92124ddcd27938ca9f3
heman <605184327@qq.com>
2021-01-03 13:07:55 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
                   'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
                   'email' => 'required|email',
                   'introduction' => 'max:100',
+                  'avatar' => 'mimes:jpg,bmp,png,gif|dimensions:min_width=208,min_height=208'
         ];
     }
 
@@ -39,6 +40,8 @@ class UserRequest extends FormRequest
                  'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
                  'name.unique' => '用户名已经存在',
                  'introduction.max' => '个人简介最多100个字符',
+                 'avatar.mimes' => '图片类型必须是jpg,bmp,png,gif',
+                 'avatar.dimensions' => '图片清晰度不够，宽和高度必须在208px以上',
         ];
     }
 }
