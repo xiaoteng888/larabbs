1870e0bb36c0d7c62de8fd80c9789d9148f7bb97
heman <605184327@qq.com>
2021-01-22 10:19:42 +0000

    用户发帖可以上传图片

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -34,7 +34,7 @@ class ImageUploadHandler
         $file->move($upload_path, $filename);
         
         // 如果限制了图片宽度，就进行裁剪
-        if($max_width && $this->allowed_ext != 'gif'){
+        if($max_width && $extension != 'gif'){
            // 此类中封装的函数，用于裁剪图片
             $this->reduceSize($upload_path . '/' . $filename, $max_width);  
         }
