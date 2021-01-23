59e34f937168326d53807217a204e175e4a1add8
heman <605184327@qq.com>
2021-01-23 15:52:41 +0000

    用户可以删除自己的帖子

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class,'user_id','id');
     }
+
+    public function isAuthOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
